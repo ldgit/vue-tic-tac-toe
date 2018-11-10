a6e9e0ed410d82219d06e6fc2d5083ca7f9bc2ed
Danko Lučić <dlucic33@gmail.com>
2018-11-10 16:52:35 +0100

    player icon display now uses correct css class

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="game-status" data-testid="gameStatus">
-    {{ description }}: <button :class=playerIconClass type="button" disabled>{{ player }}</button>
+    {{ description }}: <button :class="playerIconClass" type="button" disabled>{{ player }}</button>
   </div>
 </template>
 
 <script>
+import { getPlayerEmblemClasses } from '../helpers';
+
 export default {
-  props: ['description', 'playerIconClass', 'player', 'specialIcons'],
+  props: ['description', 'player', 'specialIcons'],
+  computed: {
+    playerIconClass: function () {
+      const { player, specialIcons } = this;
+      
+      return getPlayerEmblemClasses({ value: player, specialIcons });
+    },
+  },
 };
 </script>
